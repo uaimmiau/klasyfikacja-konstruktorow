107b1bdfc66a262f316602944d1672cdc4e640c0
Uaimmiau <zoltanchivay2137@gmail.com>
2020-05-13 23:30:01 +0200

    super ulepszenia dużo czaso chłonne

--- app.js
+++ app.js
@@ -15,7 +15,46 @@ wpisywanko = () => {
     for(i = 0; i < punktyLength; i++){
         punkty[i].innerText = tab[i].points;
     }
+    tworzenieLisineruf();
 }
+
+//LISINERY I INNE BAJERY
+const teams = document.getElementsByClassName('team');
+const racePoints = document.getElementsByClassName('racePoints');
+
+teamsEventListener = licznik => {
+    var test = 0;
+    teams[licznik].addEventListener('click', e => {
+        for(i = 0; i < racePoints.length; i++){
+            racePoints[i].style.display = "none";
+        }
+        if(test == 0){
+            racePoints[licznik].style.display = "flex";
+            test = 1;
+        }else{
+            test = 0;
+        }
+        console.log(test,"witaj o przybyszu który zauważyłeś upośledzenie systemu zamykania, to się kiedyś zateguje")
+    })
+}
+
+tworzenieLisineruf = () => {
+    for(i = 0; i < teams.length; i++){
+        teamsEventListener(i);
+    }
+    for(i = 0; i < racePoints.length; i++){
+        var racePoint = racePoints[i].querySelectorAll('p');
+        console.log(racePoint[0])
+        racePoint[0].innerText = tab[i].r1;
+        racePoint[1].innerText = tab[i].r2;
+        racePoint[2].innerText = tab[i].r3;
+        racePoint[3].innerText = tab[i].r4;
+        racePoint[4].innerText = tab[i].r5;
+        racePoint[5].innerText = tab[i].r6;
+        racePoint[6].innerText = tab[i].r7;
+    }
+}
+
 
 fetch("zespoly.json").then( res => {
     return res.json();
